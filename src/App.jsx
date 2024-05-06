1a9718e14c16f5dd15c7c925cfc48e0530d90bb9
Steve Tyler <steve.tyler@centrica.com>
2024-05-06 21:46:32 +0100

    section 151

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
 import NewProject from "./components/NewProject.jsx";
 import ProjectsSidebar from "./components/ProjectsSidebar.jsx";
+import NoProjectSelected from "./components/NoProjectSelected.jsx";
+import { useState } from 'react';
 
 function App() {
+  const [showNewProject, setShowNewProject] = useState(false);
+
+  function handleShowNewProject() {
+    setShowNewProject(true);
+  }
+
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectsSidebar />
-      <NewProject />
+      <ProjectsSidebar handleShowNewProject={handleShowNewProject}/>
+      {showNewProject ? 
+        <NewProject /> :
+        <NoProjectSelected />
+      }
     </main>
   );
 }
