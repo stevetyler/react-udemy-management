3cf98297245126ee87c59fc2d235fe4b6ca1bcd4
Steve Tyler <steve.tyler@centrica.com>
2024-05-06 20:51:27 +0100

    section 150

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
-import ProjectsSidebar from "./components/projectsSidebar";
+import NewProject from "./components/NewProject.jsx";
+import ProjectsSidebar from "./components/ProjectsSidebar.jsx";
 
 function App() {
   return (
-    <main className="h-screen my-8">
+    <main className="h-screen my-8 flex gap-8">
       <ProjectsSidebar />
+      <NewProject />
     </main>
   );
 }
