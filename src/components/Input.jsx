6aa30cc342944b28c596010ad20c780d5ce833d2
Steve Tyler <steve.tyler@centrica.com>
2024-05-08 08:29:23 +0100

    section 153

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,4 +1,6 @@
-export default function Input({ label, isTextArea, ...props }) {
+import { forwardRef } from 'react';
+
+const Input = forwardRef(function Input({ label, isTextArea, ...props }, ref) {
   const styling = "w-full p-1 border-b-2 rounded-sm border-stone-300 bg-stone-200 focus:outline-none focus:border-stone-600";
 
   return (
@@ -6,13 +8,17 @@ export default function Input({ label, isTextArea, ...props }) {
       <label className="text-sm font-bold uppercase text-stone-500">{label}</label>
       {isTextArea ?
         <textarea 
+          ref={ref}
           className={styling} 
           {...props}/> :
         <input 
+          ref={ref}
           className={styling} 
           type="text" 
           {...props}/>
       }
     </p>
   )
-}
+});
+
+export default Input;
