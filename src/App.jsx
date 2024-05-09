923207b9ac85364641369f139bd1fec2ec6a7c67
Steve Tyler <steve.tyler75@gmail.com>
2024-05-09 22:11:55 +0100

    section 158

--- src/App.jsx
+++ src/App.jsx
@@ -28,6 +28,16 @@ function App() {
     });
   }
 
+  function handleDeleteProject() {
+    setProjectsState(prevState => {
+      return {
+        ...prevState,
+        selectedProjectId: undefined,
+        projects: prevState.projects.filter(project => project.id !== prevState.selectedProjectId)
+      };
+    });
+  }
+
   function handleCancelNewProject() {
     setProjectsState(prevState => {
       return {
@@ -54,7 +64,7 @@ function App() {
   }
 
   const selectedProject = projectsState.projects.find(project => project.id == projectsState.selectedProjectId);
-  let content = <SelectedProject project={selectedProject}/>
+  let content = <SelectedProject project={selectedProject} onDelete={handleDeleteProject}/>
 
   if (projectsState.selectedProjectId === null) {
     content = <NewProject onSave={handleSaveNewProject} onCancel={handleCancelNewProject}/>
